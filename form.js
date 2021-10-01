5384fa61cef1a2f1e8c364de457c863a65f47221
tarun <tarunpandey2332@gmail.com>
2021-10-01 12:27:06 +0530

    fetch api done

--- form.js
+++ form.js
@@ -1,10 +1,14 @@
+const API_URL = "https://covid-slot-alert.herokuapp.com/"
+
 function dateHandler() {
   document.getElementById("datediv").style.display = "block";
 }
 function withOutDateHandler() {
   document.getElementById("datediv").style.display = "none";
 }
-function formValidation() {
+function formValidation(e) {
+  e.preventDefault()
+  
   const forNameRegx = /^[a-zA-Z ]{2,30}$/;
   const forPhoneRegx = /[6-9]\d{9}/;
   const name = document.getElementById("name").value;
@@ -12,8 +16,11 @@ function formValidation() {
   const pinCode = document.getElementById("pin_code").value;
   const date = document.getElementById("dates").value;
 
-    const formattedDate = date.split("-")[2] + '-' + date.split("-")[1] + '-' + date.split("-")[0];
+
+  if (date) {
+    var formattedDate = date.split("-")[2] + '-' + date.split("-")[1] + '-' + date.split("-")[0];
     console.log(formattedDate);
+  }
   
 
 
@@ -75,6 +82,47 @@ function formValidation() {
       "**please enter your valid pincode number,characters are not allowed";
     return false;
   }
+
+  console.log({name, phoneNum, pinCode, date})
+
+  postData(name, phoneNum, pinCode, date ? formattedDate : '');
   
 }
 
+function postData(name, phoneNum, pinCode, formattedDate){
+  // url=" https://covid-slot-alert.herokuapp.com/";
+//   // data="{name: name }/{phone:phoneNum}/{pin :pinCode}/{date : formattedDate}"
+//   // data="{'name': 'name','phone':'phoneNum','pin':'pinCode','date':'formattedDate' }"
+//   data=[
+//     {
+//     'name':name
+//   },
+//   {
+//     'phone':'phoneNum'
+//   },
+//   {
+//     'pin':"pinCode"
+//   },
+//   {
+//     'date':'formattedDate'
+//   }
+// ]
+
+  // para={
+  //   method:'post',
+  //   headers:{
+  //     'content-type':'application/json'
+  //   },
+  //   body:data
+  // }
+  fetch(API_URL + `register/${name}/${phoneNum}/${pinCode}/${formattedDate || ""}`).then((response)=>{
+    return response.json()
+  }).then((data)=>{
+    console.log(data)
+    if (data.msg.includes("sucess")) window.location.replace("/greeting.html")
+  })
+}
+
+const form = document.getElementById("registration-form")
+
+form.addEventListener("submit", formValidation)
